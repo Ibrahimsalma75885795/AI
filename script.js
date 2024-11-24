3486f6fbf88e7054900f717a552b5481cedbc0aa
Ibrahimsalma75885795 <ibrahimsalmanedu@gmail.com>
2024-11-24 22:21:12 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder for potential interactivity like form validation or analytics
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting us!');
+});
